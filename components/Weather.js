1a2ef8c08b47e5e5a07e30cb44bb96f1b850a3fb
AS558 <6310110558@psu.ac.th>
2022-08-27 11:16:45 +0700

    Part 4 : Connect

--- components/Weather.js
+++ components/Weather.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ImageBackground, Text, StyleSheet } from 'react-native';
 import { View } from "react-native";
 import Forecast from './Forecast';
@@ -10,6 +10,26 @@ export default function Weather(props) {
         description: '',
         temp: 0
     })
+
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+            if (props.zipCode) {
+                fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=5398f056f14b7c38ca339f35876ff25b`)
+                    .then((response) => response.json())
+                    .then((json) => {
+                        setForecastInfo({
+                            main: json.weather[0].main,
+                            description: json.weather[0].description,
+                            temp: json.main.temp
+                        });
+                     })
+                    .catch((error) => {
+                         console.warn(error);
+                    });
+            }
+        }, [props.zipCode])
+
+    
     return (
         <ImageBackground source={require('../Bg.jpg')} style={style.backdrop}>
             <View style={style.highlight}>
